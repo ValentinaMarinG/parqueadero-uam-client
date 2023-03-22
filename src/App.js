4a5bf721a99e0733c5f23dda87b6c6c0a7e9bc7a
ValentinaMarinG <84477474+ValentinaMarinG@users.noreply.github.com>
2023-03-22 11:21:53 -0500

    Revert "Modifications in the proyect´s structure (#1)"
    This reverts commit 5724dd6a5131c15002447858870bc3c9fddafe87.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.scss';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Inicialización del front-end del proyecto Parqueadero UAM</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
